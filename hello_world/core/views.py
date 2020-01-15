4ce2bebc68d8088f139f94f99531d9c4fddc239e
clear <marcospalllo@gmail.com>
2020-01-14 23:43:04 -0300

    'Projeto Hello World'

--- hello_world/core/views.py
+++ hello_world/core/views.py
@@ -2,5 +2,17 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
-def hello(request, nome, idade):
-    return HttpResponse(f'<h1>Hello {nome} de {idade} anos</h1>')
+def hello(request, nome):
+    return HttpResponse(f'<h1>Hello {nome}</h1>')
+
+def soma(request, n1, n2):
+    return HttpResponse(f'A soma de {n1} mais {n2} é {n1+n2}')
+
+def subtracao(request, n1, n2):
+    return HttpResponse(f'A subtracao de {n1} menos {n2} é {n1-n2}')
+
+def multiplicacao(request, n1, n2):
+    return HttpResponse(f'A multiplicacao entre {n1} e {n2} é {n1*n2}')
+
+def divisao(request, n1, n2):
+    return HttpResponse(f'A divisao entre {n1} e {n2} é {n1/n2}')
