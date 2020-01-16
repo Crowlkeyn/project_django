0f18c3d96cc05f49688f184d4db024e1afc0699e
clear <marcospalllo@gmail.com>
2020-01-16 00:11:01 -0300

    Autentição, login e decoradores

--- agenda/core/views.py
+++ agenda/core/views.py
@@ -1,5 +1,8 @@
+from django.contrib import messages
 from django.shortcuts import render, HttpResponse, redirect
 from core.models import Evento
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
 
 # def eventos(request, titulo_evento):
 #     return HttpResponse(f'<h1>Titulo {Evento.objects.get(titulo=titulo_evento)}</h1>')
@@ -7,10 +10,30 @@ from core.models import Evento
 # def index(request):
 #     return redirect('/agenda/')
 
+def login_user(request):
+    return render(request, 'login.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, "Usuário ou senha inválido!")
+
+    return redirect('/')
+
+@login_required(login_url='/login/')
 def lista_eventos(request):
-    # usuario = request.user
-    # evento = Evento.objects.filter(usuario=usuario)
-    evento = Evento.objects.all()
+    usuario = request.user
+    evento = Evento.objects.filter(usuario=usuario)
     dados = {'eventos':evento}
     return render(request, 'agenda.html', dados)
 
