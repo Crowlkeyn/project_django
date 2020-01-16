a1f8e45396e8a4a15aced75c503dcc1bedc1df12
clear <marcospalllo@gmail.com>
2020-01-16 15:25:39 -0300

    Realizando alterações e exclusão de dados

--- agenda/core/views.py
+++ agenda/core/views.py
@@ -1,9 +1,10 @@
 from django.contrib import messages
-from django.shortcuts import render, HttpResponse, redirect
+from django.shortcuts import render, redirect
 from core.models import Evento
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from datetime import datetime
+
 
 # def eventos(request, titulo_evento):
 #     return HttpResponse(f'<h1>Titulo {Evento.objects.get(titulo=titulo_evento)}</h1>')
@@ -11,12 +12,15 @@ from datetime import datetime
 # def index(request):
 #     return redirect('/agenda/')
 
+
 def login_user(request):
     return render(request, 'login.html')
+
 
 def logout_user(request):
     logout(request)
     return redirect('/')
+
 
 def submit_login(request):
     if request.POST:
@@ -31,26 +35,61 @@ def submit_login(request):
 
     return redirect('/')
 
+
 @login_required(login_url='/login/')
 def lista_eventos(request):
     usuario = request.user
     evento = Evento.objects.filter(usuario=usuario)
-    dados = {'eventos':evento}
+    dados = {'eventos': evento}
     return render(request, 'agenda.html', dados)
+
 
 @login_required(login_url='/login/')
 def evento(request):
-    return render(request, 'evento.html')
+    id_evento = request.GET.get('id')
+    dados = {}
+    if id_evento:
+        dados['evento'] = Evento.objects.get(id=id_evento)
+    return render(request, 'evento.html', dados)
+
 
 @login_required(login_url='/login/')
 def submit_evento(request):
     if request.POST:
         titulo = request.POST.get('titulo')
-        data_evento = datetime.strptime((request.POST.get('data')+ " " + request.POST.get('horario')), '%Y-%m-%d %H:%M')
+        data_evento = datetime.strptime((request.POST.get('data') + " " +
+                                         request.POST.get('horario')),
+                                        '%Y-%m-%d %H:%M')
+        local = request.POST.get('local')
         descricao = request.POST.get('descricao')
         usuario = request.user
-        Evento.objects.create(titulo=titulo,
-                              data_evento=data_evento,
-                              descricao=descricao,
-                              usuario=usuario)
+        id_evento = request.POST.get('id_evento')
+        if id_evento:
+            evento = Evento.objects.get(id=id_evento)
+            if evento.usuario == usuario:
+                evento.titulo = titulo
+                evento.data_evento = data_evento
+                evento.local = local
+                evento.descricao = descricao
+                evento.save()
+
+            # Evento.objects.filter(id=id_evento).update(titulo=titulo,
+            #                                            data_evento=data_evento,
+            #                                            local=local,
+            #                                            descricao=descricao)
+        else:
+            Evento.objects.create(titulo=titulo,
+                                  data_evento=data_evento,
+                                  local=local,
+                                  descricao=descricao,
+                                  usuario=usuario)
+    return redirect('/')
+
+
+@login_required(login_url='/login/')
+def delete_evento(request, id_evento):
+    usuario = request.user
+    evento = Evento.objects.get(id=id_evento)
+    if usuario == evento.usuario:
+        evento.delete()
     return redirect('/')
